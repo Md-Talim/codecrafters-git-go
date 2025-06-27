379eb57cf5c354399313b92b5564d42487b2dcba
Mohammad Talim <talimmohammad116@gmail.com>
2025-06-27 16:45:02 +0000

    feat: implement the `git init` command

--- app/main.go
+++ app/main.go
@@ -5,33 +5,34 @@ import (
 	"os"
 )
 
-// Usage: your_program.sh <command> <arg1> <arg2> ...
-func main() {
-	// You can use print statements as follows for debugging, they'll be visible when running tests.
-	fmt.Fprintf(os.Stderr, "Logs from your program will appear here!\n")
+const (
+	COMMAND_INIT = "init"
+)
 
+func initializeGitDirectory() {
+	for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
+		if err := os.MkdirAll(dir, 0755); err != nil {
+			fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
+		}
+	}
+
+	headFileContents := []byte("ref: refs/heads/main\n")
+	if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
+		fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
+	}
+
+	fmt.Println("Initialized git directory")
+}
+
+func main() {
 	if len(os.Args) < 2 {
 		fmt.Fprintf(os.Stderr, "usage: mygit <command> [<args>...]\n")
 		os.Exit(1)
 	}
 
 	switch command := os.Args[1]; command {
-	case "init":
-		// Uncomment this block to pass the first stage!
-		//
-		// for _, dir := range []string{".git", ".git/objects", ".git/refs"} {
-		// 	if err := os.MkdirAll(dir, 0755); err != nil {
-		// 		fmt.Fprintf(os.Stderr, "Error creating directory: %s\n", err)
-		// 	}
-		// }
-		//
-		// headFileContents := []byte("ref: refs/heads/main\n")
-		// if err := os.WriteFile(".git/HEAD", headFileContents, 0644); err != nil {
-		// 	fmt.Fprintf(os.Stderr, "Error writing file: %s\n", err)
-		// }
-		//
-		// fmt.Println("Initialized git directory")
-
+	case COMMAND_INIT:
+		initializeGitDirectory()
 	default:
 		fmt.Fprintf(os.Stderr, "Unknown command %s\n", command)
 		os.Exit(1)
